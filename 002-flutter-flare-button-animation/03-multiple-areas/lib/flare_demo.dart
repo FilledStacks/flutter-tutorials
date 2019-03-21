72fc91cd4bc48934d4c11d56ba0ab2aadf5bfbf5
Dane Mackier <dane.mackier@outlook.com>
2019-03-21 17:51:13 +0200

    Added step 3 folder for the tutorial

---
+++ 002-flutter-flare-button-animation/03-multiple-areas/lib/flare_demo.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flare_flutter/flare_actor.dart';
+
+class FlareDemo extends StatefulWidget {
+  @override
+  _FlareDemoState createState() => _FlareDemoState();
+}
+
+class _FlareDemoState extends State<FlareDemo> {
+  bool isOpen = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.purple,
+        body: GestureDetector(
+          onTap: (){
+            setState(() {
+             isOpen = !isOpen; 
+            });
+          },
+          child: _getAnimationUI()));
+  }
+
+  Widget _getAnimationUI() {
+    return Center(
+          child:
+              FlareActor(
+                'assets/button-animation.flr', 
+                animation: isOpen ? 'activate' : 'deactivate'),
+        );
+  }
+}
