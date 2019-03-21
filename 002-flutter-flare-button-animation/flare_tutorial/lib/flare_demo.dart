1779c8dbfb9dd5505eb8fc1c474ec18d5330d41e
Dane Mackier <dane.mackier@outlook.com>
2019-03-21 08:01:24 +0200

    Added first phase of the new tutorial

---
+++ 002-flutter-flare-button-animation/flare_tutorial/lib/flare_demo.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flare_flutter/flare_actor.dart';
+
+class FlareDemo extends StatefulWidget {
+  @override
+  _FlareDemoState createState() => _FlareDemoState();
+}
+
+class _FlareDemoState extends State<FlareDemo> {
+  bool isOpen = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.purple,
+      body: Center(
+          child: GestureDetector(
+        onTap: () {
+          setState(() {
+            isOpen = !isOpen;
+          });
+        },
+        child: FlareActor('assets/button-animation.flr',
+            animation: isOpen ? 'activate' : 'deactivate', fit: BoxFit.contain),
+      )),
+    );
+  }
+}
