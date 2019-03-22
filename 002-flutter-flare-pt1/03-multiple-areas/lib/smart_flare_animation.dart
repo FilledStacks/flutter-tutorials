1a8ab5e248d9be80dbc90f90f1c5b711cf6c053a
Dane Mackier <dane.mackier@outlook.com>
2019-03-22 19:41:38 +0200

    Updated the code for the final blog post.

--- 002-flutter-flare-pt1/03-multiple-areas/lib/smart_flare_animation.dart
+++ 002-flutter-flare-pt1/03-multiple-areas/lib/smart_flare_animation.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flare_flutter/flare_actor.dart';
 
@@ -6,7 +8,8 @@ enum AnimationToPlay {
   Deactivate,
   CameraTapped,
   PulseTapped,
-  ImageTapped
+  ImageTapped,
+  Empty
 }
 
 class SmartFlareAnimation extends StatefulWidget {
@@ -14,55 +17,55 @@ class SmartFlareAnimation extends StatefulWidget {
 }
 
 class _SmartFlareAnimationState extends State<SmartFlareAnimation> {
+  // width and height retrieved from the artboard values in the animation
   static const double AnimationWidth = 295.0;
   static const double AnimationHeight = 251.0;
 
-  bool isOpen = false;
-
   AnimationToPlay _animationToPlay = AnimationToPlay.Deactivate;
+  AnimationToPlay _lastPlayedAnimation;
+
+  bool isOpen = false;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        width: AnimationWidth,
-        height: AnimationHeight,
-        child: GestureDetector(
-            onTapUp: (tapInfo) {
-              var localTouchPosition = (context.findRenderObject() as RenderBox)
-                  .globalToLocal(tapInfo.globalPosition);
+      width: AnimationWidth,
+      height: AnimationHeight,
+      child: GestureDetector(
+          onTapUp: (tapInfo) {
+            var localTouchPosition = (context.findRenderObject() as RenderBox)
+                .globalToLocal(tapInfo.globalPosition);
 
-              var topHalfTouched = localTouchPosition.dy < AnimationHeight / 2;
+            var topHalfTouched = localTouchPosition.dy < AnimationHeight / 2;
 
-              var leftSideTouched = localTouchPosition.dx < AnimationWidth / 3;
+            var leftSideTouched = localTouchPosition.dx < AnimationWidth / 3;
 
-              var rightSideTouched =
-                  localTouchPosition.dx > (AnimationWidth / 3) * 2;
+            var rightSideTouched =
+                localTouchPosition.dx > (AnimationWidth / 3) * 2;
 
-              var middleTouched = !leftSideTouched && !rightSideTouched;
+            var middleTouched = !leftSideTouched && !rightSideTouched;
 
-              if (leftSideTouched && topHalfTouched) {
-                _setAnimationToPlay(AnimationToPlay.CameraTapped);
-              } else if (middleTouched && topHalfTouched) {
-                _setAnimationToPlay(AnimationToPlay.PulseTapped);
-              } else if (rightSideTouched && topHalfTouched) {
-                _setAnimationToPlay(AnimationToPlay.ImageTapped);
+            // Call our animation in our conditional checks
+            if (leftSideTouched && topHalfTouched) {
+              _setAnimationToPlay(AnimationToPlay.CameraTapped);
+            } else if (middleTouched && topHalfTouched) {
+              _setAnimationToPlay(AnimationToPlay.PulseTapped);
+            } else if (rightSideTouched && topHalfTouched) {
+              _setAnimationToPlay(AnimationToPlay.ImageTapped);
+            } else {
+              if (isOpen) {
+                _setAnimationToPlay(AnimationToPlay.Deactivate);
               } else {
-                if (isOpen) {
-                  _setAnimationToPlay(AnimationToPlay.Deactivate);
-                } else {
-                  _setAnimationToPlay(AnimationToPlay.Activate);
-                }
-
-                isOpen = !isOpen;
+                _setAnimationToPlay(AnimationToPlay.Activate);
               }
-            },
-            child: FlareActor(
-              'assets/button-animation.flr',
-              animation: _getAnimationName(_animationToPlay),
-              fit: BoxFit.contain,
-            )));
-  }
 
+              isOpen = !isOpen;
+            }
+          },
+          child: FlareActor('assets/button-animation.flr',
+              animation: _getAnimationName(_animationToPlay))),
+    );
+  }
 
   String _getAnimationName(AnimationToPlay animationToPlay) {
     switch (animationToPlay) {
@@ -78,13 +81,30 @@ class _SmartFlareAnimationState extends State<SmartFlareAnimation> {
         return 'image_tapped';
         break;
       default:
-        return 'deactivate';
+        return '';
     }
   }
 
   void _setAnimationToPlay(AnimationToPlay animation) {
     setState(() {
-      _animationToPlay = animation;
+
+      var isTappedAnimation = _getAnimationName(animation).contains("_tapped");
+      if(isTappedAnimation && _lastPlayedAnimation == AnimationToPlay.Deactivate) {
+        return;
+      }
+
+      if (animation == _animationToPlay) {
+        _animationToPlay = AnimationToPlay.Empty;
+        Timer(const Duration(milliseconds: 50), () {
+          setState(() {
+            _animationToPlay = animation;
+          });
+        });
+      } else {
+        _animationToPlay = animation;
+      }
+
+      _lastPlayedAnimation = animation;
     });
   }
 }
