6f31ab994fe53c80af2e7aec4e51736785fc972f
Dane Mackier <dane.mackier@outlook.com>
2019-03-23 08:38:19 +0200

    Update the purple to match the expected one

--- 002-flutter-flare-pt1/01-start/lib/flare_demo.dart
+++ 002-flutter-flare-pt1/01-start/lib/flare_demo.dart
@@ -9,7 +9,7 @@ class _FlareDemoState extends State<FlareDemo> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.purple,
+      backgroundColor: Color.fromARGB(255, 102, 18, 222),
       body: Center(child: Text('Flare Demo')),
     );
   }
