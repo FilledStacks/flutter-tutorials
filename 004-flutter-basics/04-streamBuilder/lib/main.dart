df560bf0cedcb4eadf4676168e89aaa5ddcb7617
Dane Mackier <dane.mackier@outlook.com>
2019-03-29 07:52:09 +0200

    Added the StreamBuilder code.

---
+++ 004-flutter-basics/04-streamBuilder/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import './home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Home()
+    );
+  }
+}
