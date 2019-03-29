a5cb13bf99f6c918221993be3172afee22ae7cb6
Dane Mackier <dane.mackier@outlook.com>
2019-03-29 07:51:13 +0200

    Added the final FutureBuilder project code.

--- 004-flutter-basics/03-futureBuilder/lib/home.dart
+++ 004-flutter-basics/03-futureBuilder/lib/home.dart
@@ -1,69 +1,74 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-
-  List<String> _pageData;
-
-  bool get _fetchingData => _pageData == null;
-
-  @override
-  void initState() {
-    _getListData(hasError: true).then((data) => 
-        setState(() {
-          if(data.length == 0) {
-            data.add('No data found for your account. Add something and check back.');
-          }
-          _pageData = data;
-        }))
-        .catchError((error) => 
-        setState((){
-          _pageData = [error];
-        }));
-    super.initState();
-  }
-
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[900],
-      body: _fetchingData 
-      ? Center(child: CircularProgressIndicator())
-      : ListView.builder(
-        itemCount: _pageData.length,
-        itemBuilder: (buildContext, index) =>_getListItemUi(index)
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        // We want to refresh, but this actually does nothing. Which is the limitation
+        _getListData();
+      }),
+        backgroundColor: Colors.grey[900],
+        body: FutureBuilder(
+          future: _getListData(),
+          builder: (buildContext, snapshot) {
+
+            if(snapshot.hasError) {
+              return _getInformationMessage(snapshot.error);
+            }
+
+            if (!snapshot.hasData) {
+              return Center(child: CircularProgressIndicator());
+            }
+
+            var listItems = snapshot.data;
+            if(listItems.length == 0) {
+              return _getInformationMessage('No data found for your account. Add something and check back.');
+            }
+
+            return ListView.builder(
+                itemCount: listItems.length,
+                itemBuilder: (buildContext, index) => _getListItemUi(index, listItems));
+          }
+        ));
+  }
+
+  Widget _getListItemUi(int index, List<String> listItems) {
+    return Container(
+      margin: EdgeInsets.all(5.0),
+      height: 50.0,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5.0), color: Colors.grey[600]),
+      child: Center(
+        child: Text(
+          listItems[index],
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _getInformationMessage(String message) {
+    return Center(
+        child: Text(
+      message,
+      textAlign: TextAlign.center,
+      style: TextStyle(fontWeight: FontWeight.w900, color: Colors.grey[500]),
     ));
   }
 
-  Widget _getListItemUi(int index) {
-    return  Container(
-          margin: EdgeInsets.all(5.0),
-          height: 50.0,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(5.0),
-            color: Colors.grey[600]
-          ),
-          child: Center(
-            child: Text(_pageData[index], style: TextStyle(color: Colors.white,),
-        ),
-          ),
-      );
-  }
-
-  Future<List<String>> _getListData({
-    bool hasError = false,
-    bool hasData = true}) async {
+  Future<List<String>> _getListData(
+      {bool hasError = false, bool hasData = true}) async {
     await Future.delayed(Duration(seconds: 2));
 
-    if(hasError) {
-      return Future.error('An error occurred while fetching the data. Please try again later.');
+    if (hasError) {
+      return Future.error(
+          'An error occurred while fetching the data. Please try again later.');
     }
 
-    if(!hasData) {
+    if (!hasData) {
       return List<String>();
     }
 
