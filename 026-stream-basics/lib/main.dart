568f629e640aca927ae503c8fa119ecee3058318
Dane Mackier <dane.mackier@outlook.com>
2019-09-09 06:47:15 +0200

    Added the final code for the stream basics tutorial.

---
+++ 026-stream-basics/lib/main.dart
@@ -0,0 +1,63 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  StreamController<double> controller = StreamController<double>();
+  StreamSubscription<double> streamSubscription;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        home: Scaffold(
+          body: Center(
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                MaterialButton(
+                  child: Text('Subscribe'),
+                  color: Colors.yellow,
+                  onPressed: () async {
+                    getDelayedRandomValue().listen((value) {
+                      print('Value from manualStream: ${value}');
+                    });
+                  },
+                ),
+                MaterialButton(
+                  child: Text('Emit Value'),
+                  color: Colors.blue[200],
+                  onPressed: () {
+                    controller.add(12);
+                  },
+                ),
+                MaterialButton(
+                  child: Text('Unsubscribe'),
+                  color: Colors.red[200],
+                  onPressed: () {
+                    streamSubscription?.cancel();
+                  },
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+
+  Stream<double> getDelayedRandomValue() async* {
+    var random = Random();
+
+    while (true) {
+      await Future.delayed(Duration(seconds: 1));
+      yield random.nextDouble();
+    }
+  }
+}
--- README.md
+++ README.md
@@ -9,6 +9,8 @@ Please clone and star this repo to stay up to date on changes.
 
 ## Tutorials
 
+**026 - Flutter stream basics. Absolute basics** \[ [Video](https://youtu.be/53jIxLiCv2E) \] \[ [Written](https://www.filledstacks.com/post/a-complete-guide-to-flutter-streams) \] - This is a short tutorial that covers the creation, usage and management of streams. I also cover how to manually create Streams using `async*` functionality.
+
 **025 - Flutter navigation without BuildContext | Navigation Service** \[ [Video](https://youtu.be/kopdISefbJc) \] \[ [Written](http://filledstacks.com/post/navigate-without-build-context-in-flutter-using-a-navigation-service) \] - This tutorial covers the creation of a Navigation Service to move your navigation logic out of your UI files into the business logic files.
 
 **024 - Abstraction in Flutter Pt2 | Better unit tests using Abstraction** \[ [Video](https://youtu.be/oZW3Eb3J9s0) \] \[ [Written](https://www.filledstacks.com/post/better-unit-tests-in-flutter-using-abstraction) \] - In this tutorial we go over how using Abstraction can make your unit tests better.
