2b176075dfd9f9a15623a8e10a43b21165df0426
Dane Mackier <dane.mackier@outlook.com>
2019-11-24 14:02:00 +0200

    Updated the starting code for the season details

--- 035-statemanagement-api-integration/00-starting/lib/widgets/season_details/season_details.dart
+++ 035-statemanagement-api-integration/00-starting/lib/widgets/season_details/season_details.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 import 'package:the_basics/datamodels/season_details_model.dart';
-import 'package:the_basics/styles/text_styles.dart';
+import 'package:the_basics/widgets/season_details/season_details_desktop.dart';
+import 'package:the_basics/widgets/season_details/season_details_mobile.dart';
 
 class SeasonDetails extends StatelessWidget {
   final SeasonDetailsModel details;
@@ -10,64 +11,8 @@ class SeasonDetails extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenTypeLayout(
-      desktop: _SeasonDetailsDesktop(details: details),
-      mobile: _SeasonDetailsMobile(details: details),
-    );
-  }
-}
-
-class _SeasonDetailsDesktop extends StatelessWidget {
-  final SeasonDetailsModel details;
-  const _SeasonDetailsDesktop({Key key, this.details}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return ResponsiveBuilder(
-      builder: (context, sizingInformation) => Row(
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          Text(
-            details.title,
-            style: titleTextStyle(sizingInformation.deviceScreenType),
-          ),
-          SizedBox(
-            width: 50,
-          ),
-          Expanded(
-            child: Text(
-              details.description,
-              style: descriptionTextStyle(sizingInformation.deviceScreenType),
-            ),
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class _SeasonDetailsMobile extends StatelessWidget {
-  final SeasonDetailsModel details;
-  const _SeasonDetailsMobile({Key key, this.details}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return ResponsiveBuilder(
-      builder: (context, sizingInformation) => Column(
-        children: <Widget>[
-          Text(
-            details.title,
-            style: titleTextStyle(sizingInformation.deviceScreenType),
-          ),
-          SizedBox(
-            height: 50,
-          ),
-          Text(
-            details.description,
-            style: descriptionTextStyle(sizingInformation.deviceScreenType),
-          ),
-        ],
-      ),
+      desktop: SeasonDetailsDesktop(details: details),
+      mobile: SeasonDetailsMobile(details: details),
     );
   }
 }
