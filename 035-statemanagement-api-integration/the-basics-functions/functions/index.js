c2a9f27ce0576608ed6aaabd812af25b59280008
Dane Mackier <dane.mackier@outlook.com>
2019-12-11 18:26:11 +0200

    035-Updated the image urls

--- 035-statemanagement-api-integration/the-basics-functions/functions/index.js
+++ 035-statemanagement-api-integration/the-basics-functions/functions/index.js
@@ -8,34 +8,34 @@ app.use(cors());
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 
-app.get('/courseEpisodes',  (req, res, next) => {
-   var episodes = [
+app.get("/courseEpisodes", (req, res, next) => {
+  var episodes = [
     {
       title: "Setup, Build and Deploy",
       duration: 14.07,
       imageUrl:
-        "https://www.filledstacks.com/assets/static/32.81b85c1.ebb7a1a.jpg"
+        "https://www.filledstacks.com/assets/static/32.81b85c1.ebb7a1aaddfef10313d7bab97243d449.jpg"
     },
     {
       title: "Adding a Responsive UI",
       duration: 18.54,
       imageUrl:
-        "https://www.filledstacks.com/assets/static/033.81b85c1.ebdf16d.jpg"
+        "https://www.filledstacks.com/assets/static/033.81b85c1.ebdf16d08a54fd37bc4cd5d1a0b0767f.jpg"
     },
     {
       title: "Layout Templates",
       duration: 14.55,
       imageUrl:
-        "https://www.filledstacks.com/assets/static/034.81b85c1.52d0785.jpg"
+        "https://www.filledstacks.com/assets/static/034.81b85c1.52d07852af687a77985f54c5eedecb82.jpg"
     },
     {
       title: "State Management and Api integration",
       duration: 14.55,
       imageUrl:
-        "https://www.filledstacks.com/assets/static/034.81b85c1.52d0785.jpg"
+        "https://www.filledstacks.com/assets/static/35.81b85c1.f9ab74c22bb56931e36a13783c29361f.jpg"
     }
   ];
-  
+
   res.json(episodes);
 });
 
