5831a866729cf81091823d29482a7b79abc645c2
Dane Mackier <dane.mackier@outlook.com>
2019-12-15 18:40:44 +0200

    037-Finished up the starting code for the navigation tutorial

---
+++ 037-advanced-web-navigation/00-starting/lib/viewmodels/episode_list_view_model.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/cupertino.dart';
+
+class EpisodeListViewModel extends ChangeNotifier {}
--- 037-advanced-web-navigation/00-starting/lib/widgets/episodes_list/episodes_list.dart
+++ 037-advanced-web-navigation/00-starting/lib/widgets/episodes_list/episodes_list.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider_architecture/viewmodel_provider.dart';
 import 'package:the_basics/datamodels/episode_item_model.dart';
+import 'package:the_basics/viewmodels/episode_list_view_model.dart';
 
 import 'episode_item.dart';
 
@@ -9,14 +11,15 @@ class EpisodesList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Wrap(
-      spacing: 30,
-      runSpacing: 30,
-      children: <Widget>[
-        ...episodes.map(
-          (episode) => EpisodeItem(model: episode),
-        )
-      ],
+    return ViewModelProvider<EpisodeListViewModel>.withConsumer(
+      viewModel: EpisodeListViewModel(),
+      builder: (context, model, child) => Wrap(
+        spacing: 30,
+        runSpacing: 30,
+        children: <Widget>[
+          ...episodes.map((episode) => EpisodeItem(model: episode)).toList()
+        ],
+      ),
     );
   }
 }
