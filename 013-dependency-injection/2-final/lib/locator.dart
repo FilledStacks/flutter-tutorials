a1da2d75fcc3a2c2207adbcfaf43a6345dc28dc1
bsneider <54788551+bsneider@users.noreply.github.com>
2020-06-18 19:55:39 -0500

    update getit

--- 013-dependency-injection/2-final/lib/locator.dart
+++ 013-dependency-injection/2-final/lib/locator.dart
@@ -2,7 +2,7 @@ import 'package:get_it/get_it.dart';
 
 import 'app_info.dart';
 
-GetIt locator = GetIt();
+GetIt locator = GetIt.instance;
 
 void setupLocator() {
   locator.registerFactory(() => AppInfo());
--- 022-lifecycle-manager/lib/locator.dart
+++ 022-lifecycle-manager/lib/locator.dart
@@ -1,9 +1,8 @@
-
 import 'package:get_it/get_it.dart';
 import 'package:lifecycle_manager/services/background_fetch_service.dart';
 import 'package:lifecycle_manager/services/location_service.dart';
 
-GetIt locator = GetIt();
+GetIt locator = GetIt.instance;
 
 void setupLocator() {
   locator.registerLazySingleton(() => LocationService());
